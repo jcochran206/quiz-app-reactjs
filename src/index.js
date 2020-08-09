a4b028f4c749407d85268e1aaf2b449a53492f97
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2020-08-09 15:10:56 -0700

    deleted files and created container

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "./assets/style.css";
+import Questions from './questionBank/questionService'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Quiz extends Component {
+  state = {
+    questionBank: [] 
+  };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  getQuestions = () => {
+    Questions().then(question => {
+      this.setState({
+        questionBank: question
+      });
+    });
+  };
+
+  componentDidMount() {
+    this.getQuestions();
+  }
+
+  render(){
+    return (
+      <div className='container'>
+        <div className='title'><h1>Quiz</h1></div>
+    {this.state.questionBank.length > 0 && this.state.questionBank.map(({question, answers, correct, questionId}) => (<h4>{question}</h4>))}
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Quiz/>, document.getElementById('root'));
